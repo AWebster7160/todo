24c2530276a744bf618f13dfc28c572ef7891764
AWebster7160 <156373395+AWebster7160@users.noreply.github.com>
2024-02-16 17:32:05 -0500

    Main div mock-up

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # todo
 TOP Todo List project implementing SOLID principles
+
+2/16/2024 - As stated in desc. Will again use webpack to bundle. First using external libraries and API (Web Storage). Also will be first time implementing dark/light theme switching.
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './src',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+],
+};
